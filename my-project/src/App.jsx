9cbf3b7cc028457de2db9140b6981a64bd41b7a0
Kelechi <kanomneze@gmail.com>
2023-07-15 12:44:20 +0100

    init

---
+++ my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import BackendUseEffect from "./components/BackendUseEffects";
+import WindowWidth from "./components/WindowWidth";
+
+function App() {
+  return (
+    <>
+      <BackendUseEffect />
+      <WindowWidth />
+    </>
+  );
+}
+
+export default App;
